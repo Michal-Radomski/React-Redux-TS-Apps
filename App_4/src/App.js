b17a87072955c026d3dca1a54a6b2cb19eb920c2
Michal Radomski <michalra@op.pl>
2021-10-11 22:23:53 +0200

    App_4 done

--- App_4/src/App.js
+++ App_4/src/App.js
@@ -1,8 +1,75 @@
 import React from "react";
+import {connect} from "react-redux";
 import "./App.scss";
 
-function App() {
-  return <React.Fragment>App</React.Fragment>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: "",
+      postId: -2,
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => {
+        this.props.dispatch({
+          type: "LOAD_POSTS",
+          payload: json,
+        });
+      });
+  }
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+  };
+
+  handleSubmit = (event) => {
+    // alert("Content submitted: " + this.state.value);
+    event.preventDefault();
+    this.props.dispatch({
+      type: "ADD_POST",
+      payload: {id: this.state.postId, title: this.state.value},
+    });
+
+    this.setState({postId: this.state.postId - 1});
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        App:
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <div>
+            <button type="submit" onClick={this.handleSubmit}>
+              Submit
+            </button>
+          </div>
+        </form>
+        <ul>
+          {this.props.posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {posts: state.posts};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatch,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ App_4/src/store/index.js
@@ -0,0 +1,77 @@
+// Redux Store
+
+import {createStore} from "redux";
+
+// State
+const initialState = {
+  posts: [{id: -1, title: "Test Post"}],
+  loginModal: {
+    open: false,
+  },
+};
+
+// Reducer
+const reducer = (state = initialState, action) => {
+  if (action.type === "ADD_POST") {
+    return Object.assign({}, state, {
+      posts: state.posts.concat(action.payload),
+    });
+  }
+
+  if (action.type === "LOAD_POSTS") {
+    return {
+      ...state,
+      posts: state.posts.concat(action.payload),
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(
+  reducer,
+  // Below tine to connect the Store with Redux DevTools
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
+
+// + createStore API
+//- getState for accessing the current state of the application
+//- dispatch for changing state via an action
+//- subscribe for responding to state changes
+
+//+ A reducer is just a function that takes two arguments and returns your app's current state. The two arguments it takes are:
+//- Your current state (JavaScript object)
+//- An action (also a JavaScript object)
+
+//+ Redux actions are JavaScript objects that contain two properties: type and payload.
+//- These actions are "dispatched," or used as arguments by the Redux store's dispatch API method.
+
+// + Redux action:
+//- {
+//-   type: 'ADD_POST',
+//-   payload: { id: 1, title: 'Redux Tutorial 2019' }
+//- }
+
+// + Dispatch (Updating) the State:
+//- store.dispatch({ type: 'ADD_POST', payload: { id: 0, title: 'How to Use Redux' } })
+
+//+ Print the State:
+//- store.getState();
+
+// -react-redux provides a component called Provider and a function called connect.
+// -The Provider component wraps around React's root App component.
+// -We pass our store through as a prop to "provide" it to all components within our app
+
+//+ We can now access our state directly within components using react-redux's connect function
+//+ The connect function takes another function as an argument: mapStateToProps.
+//- mapStateToProps determines what state from our store we want to pull into our component.
+//- In this case, we're specifying to only pull our state's posts property
+//+ By creating a mapDispatchToProps function and passing it as the second argument to connect, we're making our store's
+//- dispatch method available for use within our component's this.props object
+
+//+ Redux-Thunk is a library meant for handling action creators, functions meant solely to return "actions," JavaScript objects with a updated state data:
+//- /Can be used Fetch Method/
+//- Setup your store's reducer to handle a new action
+//- Fetch the data and dispatch a corresponding action
