8b8e80135094f59df01c2c29d47d9769f523ca65
Michal Radomski <michalra@op.pl>
2021-10-30 01:24:04 +0200

    App_14 - commit1

--- App_14 CovidTracker/src/App.js
+++ App_14 CovidTracker/src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
-import "./App.scss";
 
-function App() {
-  return <React.Fragment>App</React.Fragment>;
+import styles from "./App.module.scss";
+
+// import Cards from "./components/Cards/Cards"
+// import Chart from "./components/Chart/Chart"
+// import CountryPicker from "./components/CountryPicker/CountryPicker"
+// - Instead of above import
+import {Cards, Chart, CountryPicker} from "./components/index";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <Chart />
+        <CountryPicker />
+      </div>
+    );
+  }
 }
 
 export default App;
