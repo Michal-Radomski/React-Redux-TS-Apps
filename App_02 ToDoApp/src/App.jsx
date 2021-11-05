da1b0cda7ffb540f5f3f08b90b56ad5a81c5a0d1
Michal Radomski <michalra@op.pl>
2021-11-05 18:12:41 +0100

    App_2 done

---
+++ App_02 ToDoApp/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {connect} from "react-redux";
+
+import "./App.scss";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import {deleteAll, persistTodos} from "./redux/actions";
+
+const App = ({deleteAll, persistTodos}) => {
+  React.useEffect(() => {
+    persistTodos();
+  }, [persistTodos]);
+
+  return (
+    <div className="App">
+      <h1 style={{textDecoration: "underline"}}>Todos App</h1>
+      <InputField />
+      <TodoList />
+      <div>
+        <button style={{marginTop: "20px", cursor: "pointer"}} onClick={deleteAll}>
+          Delete All
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  deleteAll: () => dispatch(deleteAll()),
+  persistTodos: () => dispatch(persistTodos()),
+});
+
+export default connect(null, mapDispatchToProps)(App);
---
+++ App_02 ToDoApp/src/redux/store.js
@@ -0,0 +1,6 @@
+import {createStore, applyMiddleware} from "redux";
+import logger from "redux-logger";
+
+import {addTodo} from "./reducers";
+
+export const store = createStore(addTodo, applyMiddleware(logger));
