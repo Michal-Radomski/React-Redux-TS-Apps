a9b6aa49a0802386ee89076c6e42f5a973fbe118
Michal Radomski <michalra@op.pl>
2021-11-05 18:48:00 +0100

    temp commit2

--- App_02 ToDoApp/src/redux/store.js
+++ App_02 ToDoApp/src/redux/store.js
@@ -1,8 +1,9 @@
 import {createStore, applyMiddleware} from "redux";
 import logger from "redux-logger";
+import {composeWithDevTools} from "redux-devtools-extension";
 
-import {addTodo} from "./reducers";
+import {addTodo} from "./reducers"; //rootReducer
 
-export const store = createStore(addTodo, applyMiddleware(logger));
+export const store = createStore(addTodo, composeWithDevTools(applyMiddleware(logger)));
 
-console.log(store.getState());
+// console.log(store.getState());
